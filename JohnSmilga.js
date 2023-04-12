70393973faf4fdfad00284d2171ea7a16613f646
Walusimbi Herbert Henry <walherb@gmail>
2023-04-12 15:23:04 +0300

    JavaScript Nuggets

--- JohnSmilga.js
+++ JohnSmilga.js
@@ -43,10 +43,39 @@
 
 // Book List
 // JSX - JavaScript
-// { Video: Javascript Nuggets - Destructuring (object) }
+//! { Video: Javascript Nuggets - Destructuring (object) }
+/*
+#objectdestructuring
+const bob = {
+  first: 'bob',
+  last: 'sanders',
+  city: 'chicago',
+  siblings: {
+    sister: 'jane',
+  },
+};
 
-// { Video: Javascript Nuggets - Map } moment --> npm package for dates & time
+// const firstName = bob.first;
+// const lastName = bob.last;
+// const sister = bob.siblings.sister;
+
+// const { first, last, city } = bob;
+// const { first:aliasOfFirst, last, city } = bob;
+// console.log(aliasOfFirst,last, city);
+const {
+  first,
+  last,
+  city,
+  siblings: { sister: favoriteSibling },
+} = bob;
+console.log(first, last, city, favoriteSibling);
+#objectdestructuring
+*/
+
 // https://www.youtube.com/watch?v=f_sEWa5hA0Q
+//! { Video: Javascript Nuggets - Map } moment --> npm package for dates & time
+/*
+#map
 const people = [
   {
     name: 'bob',
@@ -86,11 +115,116 @@ console.log(newPeople);
 const names = people.map((person) => `<h2>${person.name}</h2>`);
 const result = document.querySelector('#result');
 result.innerHTML = names.join('');
+#map
+ */
 
-// { Video: Javascript Nuggets - Spread Operator }
 // [ React Events] (https://reactjs.org/docs/events.html)
-// [ Video: JavaScript Nuggets - Destructuring (Array) ]
-// { Video: Javascript Nuggets - Filter and Find }
+//! { Video: Javascript Nuggets - Spread Operator } ...
+/*
+#spreadoperator
+const udemy = 'udemy';
+const letters = [...udemy];
+console.log(letters);
+
+const boys = ['john', 'peter', 'bob'];
+const girls = ['susan', 'anna'];
+
+const bestFriend = 'arnold';
+
+const friends = [...boys, ...girls, bestFriend];
+
+console.log(friends);
+
+// reference
+// const newFriends = friends;
+// copy
+const newFriends = [...friends];
+newFriends[0] = 'nancy';
+console.log(friends);
+console.log(newFriends);
+
+// ES2018 - ES8 Objects
+
+const person = { name: 'john', job: 'developer' };
+const newPerson = { ...person, city: 'chicago', name: 'peter' };
+console.log(person);
+console.log(newPerson);
+#spreadoperator
+*/
+
+//! [ Video: JavaScript Nuggets - Destructuring (Array) ]
+/*
+#arraydestructuring
+const fruits = ['orange', 'banana', 'lemon'];
+const friends = ['john', 'peter', 'bob', 'anna', 'kelly'];
+
+// const fruit1 = fruits[0];
+// const fruit2 = fruits[1];
+// const fruit3 = fruits[2];
+// console.log(fruit1, fruit2, fruit3);
+
+// const [john, peter, bob, anna, kelly] = friends;
+// console.log(john, peter, bob, anna, kelly);
+const [enemy, , bob, , kelly, susan] = friends;
+console.log(enemy, bob, kelly, susan);
+
+let first = 'bob';
+let second = 'john';
+
+// let temp = second;
+// second = first;
+// first = temp;
+// console.log(first, second);
+
+[second, first] = [first, second];
+console.log(first, second);
+#arraydestructuring
+*/
+
+// FusionCharts
+//! { Video: Javascript Nuggets - Filter and Find }
+/*
+#filterandfind
+const people = [
+  { name: 'bob', age: 20, position: 'developer' },
+  { name: 'peter', age: 25, position: 'designer' },
+  { name: 'susy', age: 30, position: 'the boss' },
+  { name: 'anna', age: 35, position: 'intern' },
+];
+const fruits = ['orange', 'pear', 'lemon'];
+// filter
+const youngPeople = people.filter((person) => {
+  // if (person.age < 30) {
+  //   return person;
+  // }
+  return person.age < 30;
+});
+console.log(youngPeople);
+const developers = people.filter((person) => person.position === 'developer');
+console.log(developers);
+
+// no match
+const seniorDevs = people.filter((item) => item.position === 'senior dev');
+console.log(seniorDevs);
+
+// find
+const peter = people.find((person) => person.name === 'peter');
+console.log(peter);
+const fruit = fruits.find((fruit) => fruit === 'lemon');
+console.log(fruit);
+// no match
+const oldPerson = people.find((person) => person.age > 35);
+console.log(oldPerson);
+
+// multiple matches - first match
+const randomPerson = people.find((person) => person.age < 35);
+console.log(randomPerson);
+
+const anna = people.filter((person) => person.name === 'anna');
+console.log(peter.position); // find
+console.log(anna[0].position); // filter
+#filterandfind
+*/
 
 // npm run build
 // netlify.com: { magical-vacherin-4bf4c3 }
